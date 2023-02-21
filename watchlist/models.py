a3b66049cf0b33f5ff454bf323728caf794d96b9
lijialiang <614237720@qq.com>
2023-02-21 17:47:18 +0800

    fix

--- watchlist/models.py
+++ watchlist/models.py
@@ -37,4 +37,7 @@ class Contact(db.Model):
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
-    
+
+    def __repr__(self) -> str:
+        return '<Note %r>' %self.body
+
